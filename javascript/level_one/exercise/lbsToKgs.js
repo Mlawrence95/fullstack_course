6b20c5238a859812e35979933de37990e4e20d6e
Michael Lawrence <mikelawrence@berkeley.edu>
2020-04-16 22:43:29 -0700

    basic js exercise

---
+++ javascript/level_one/exercise/lbsToKgs.js
@@ -0,0 +1,7 @@
+var lbs = prompt("What is your weight in pounds?");
+
+kgs = lbs * 0.454;
+// this is a *backtick*
+alert(`Your weight in kilograms is ${kgs}`);
+
+console.log("Conversion completed.");
