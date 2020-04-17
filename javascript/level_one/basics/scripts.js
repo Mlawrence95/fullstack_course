e030b2e1091aafbfd275ee57c891b73351d247ef
Michael Lawrence <mikelawrence@berkeley.edu>
2020-04-16 22:32:52 -0700

    basic js script

---
+++ javascript/level_one/basics/scripts.js
@@ -0,0 +1,6 @@
+alert("WelcoOoOoOomEE! :^)")
+
+var favStardewCharacter = prompt("Who is your favorite Stardew character?")
+alert("Your fav character is " + favStardewCharacter)
+
+console.log("yeeea we play stardew");
